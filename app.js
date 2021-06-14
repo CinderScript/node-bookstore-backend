582ac0c7345dcb6f6f6d5e7a33be62dce8b34bef
maynag <70300031+maynag@users.noreply.github.com>
2021-06-13 19:10:35 -0700

    cors try 2

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.options('*', cors()) // include before other routes
+//app.options('*', cors()) // include before other routes
 
 app.use(cors());
 
